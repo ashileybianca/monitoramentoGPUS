1175b07f027a08090a61696bb14aef09cff7098f
ashileybianca <ashiley.bianca@edu.pucrs.br>
2023-08-08 16:57:17 -0300

    Começo do desenvolvimento

--- monitor_gpu.py
+++ monitor_gpu.py
@@ -1,14 +1,28 @@
 import subprocess
 import time
+import re
 
 def collect_gpu_info():
     try:
         output = subprocess.check_output(['nvidia-smi'], universal_newlines=True)
         return output
-
     except Exception as e:
-        print("Error: ", e)
+        print("Error:", e)
         return None
 
-print(collect_gpu_info())
+def extract_gpu_info(gpu_info):
 
+    for caractere in gpu_info:
+
+        match = re.match(r'(\d+%) +(\d+)C +(\d+)W / (\d+)W +(\d+)MiB / (\d+)MiB')
+        if match:
+            fan, temp, pwr_usage, _, mem_used = match.groups()
+            print("Fan:", fan)
+            print("Temperature:", temp)
+            print("Power Usage:", pwr_usage)
+            print("Memory Used:", mem_used)
+            gpu_data.append((gpu_name, fan, temp, pwr_usage, mem_used))
+
+    return gpu_data
+
+print(extract_gpu_info(collect_gpu_info()))
