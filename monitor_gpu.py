ce3fbb5dede547a1f1a6a833adb8f0329e879079
ashileybianca <ashiley.bianca@edu.pucrs.br>
2023-08-10 15:03:09 -0300

    Peak values e gpu info atualizados, funcionando!

--- monitor_gpu.py
+++ monitor_gpu.py
@@ -1,7 +1,6 @@
 import subprocess
 from datetime import datetime
 import time
-import re
 
 def get_gpu_info():
     try:
@@ -23,46 +22,75 @@ def parse_gpu_info(output):
     lines = output.strip().split('\n')
     for line in lines:
         values = line.split(', ')
-        temperature_str = re.findall(r'\d+', values[3])[0]  # Extrair o primeiro número da string
         gpu = {
             'Index': int(values[0]),
             'Name': values[1],
             'UUID': values[2],
-            'Temperature': int(temperature_str),
-            'Fan Speed': int(values[4].split()[0]),
-            'Power Draw': float(values[5].split()[0]),
-            'Power Limit': float(values[6].split()[0]),
+            'Temperature': int(values[3].replace('°C', '')),
+            'Fan Speed': int(values[4].replace(' %', '')),
+            'Power Draw': float(values[5].replace(' W', '')),
+            'Power Limit': float(values[6].replace(' W', '')),
             'Memory Used': int(values[7].split()[0]),
             'Memory Total': int(values[8].split()[0])
         }
         gpu_list.append(gpu)
     return gpu_list
 
-while True:
-    timestamp, output = get_gpu_info()
-    if output is not None:
-        gpu_info = parse_gpu_info(output)
+def find_peak_values(gpu_data, peak_values):
+    timestamp = gpu_data[0]['Timestamp']
 
-        with open('gpu_info.txt', 'a') as file:
-            file.write("=================================================\n")
-            file.write(f"GPU Information (Timestamp: {timestamp}):\n\n")
+    for gpu in gpu_data:
+        gpu_index = gpu['Index']
+        if gpu_index not in peak_values:
+            peak_values[gpu_index] = {
+                'Temperature': {'Value': gpu['Temperature'], 'Time': timestamp},
+                'Fan Speed': {'Value': gpu['Fan Speed'], 'Time': timestamp},
+                'Power Draw': {'Value': gpu['Power Draw'], 'Time': timestamp},
+            }
+        else:
+            if gpu['Temperature'] > peak_values[gpu_index]['Temperature']['Value']:
+                peak_values[gpu_index]['Temperature'] = {'Value': gpu['Temperature'], 'Time': timestamp}
+            if gpu['Fan Speed'] > peak_values[gpu_index]['Fan Speed']['Value']:
+                peak_values[gpu_index]['Fan Speed'] = {'Value': gpu['Fan Speed'], 'Time': timestamp}
+            if gpu['Power Draw'] > peak_values[gpu_index]['Power Draw']['Value']:
+                peak_values[gpu_index]['Power Draw'] = {'Value': gpu['Power Draw'], 'Time': timestamp}
+    return peak_values
+
+output_filename = "gpu_logs.txt"
+
+def main():
+    peak_values = {}
+    while True:
+        timestamp, output = get_gpu_info()
+        if output is not None:
+            gpu_info = parse_gpu_info(output)
+            gpu_info[0]['Timestamp'] = timestamp 
+
+            formatted_output = f"=================================================\n"
+            formatted_output += f"GPU Information (Timestamp: {timestamp}):\n"
             for gpu in gpu_info:
-                file.write(f"GPU-{gpu['Index']} ({gpu['Name']}):\n")
-                file.write(f"  UUID: {gpu['UUID']}\n")
-                file.write(f"  Temperature: {gpu['Temperature']}°C\n")
-                file.write(f"  Fan Speed: {gpu['Fan Speed']} %\n")
-                file.write(f"  Power Draw: {gpu['Power Draw']} W / Power Limit: {gpu['Power Limit']} W\n")
-                file.write(f"  Memory Used: {gpu['Memory Used']} MiB / Memory Total: {gpu['Memory Total']} MiB\n\n")
+                formatted_output += f"\nGPU-{gpu['Index']} ({gpu['Name']}):\n"
+                formatted_output += f"  UUID: {gpu['UUID']}\n"
+                formatted_output += f"  Temperature: {gpu['Temperature']}°C\n"
+                formatted_output += f"  Fan Speed: {gpu['Fan Speed']} %\n"
+                formatted_output += f"  Power Draw: {gpu['Power Draw']} W\n"
+                formatted_output += f"  Memory Used: {gpu['Memory Used']} MiB\n"
 
-    time.sleep(300)  # 5 minutos
-    filename = 'gpu_info.txt'
-    gpu_data = load_data(filename)
-    peak_values = find_peak_values(gpu_data)
+            peak_values = find_peak_values(gpu_info, peak_values)
 
-    for gpu, values in peak_values.items():
-        print(f"GPU {gpu} - Peak Values:")
-        print(f"  Temperature: {values['Temperature']}°C")
-        print(f"  Fan Speed: {values['Fan Speed']} %")
-        print(f"  Power Draw: {values['Power Draw']} W")
-        print(f"  Memory Used: {values['Memory Used']} MiB")
-        print()
+            formatted_output += f"\nPEAK VALUES (until Timestamp: {timestamp}):\n"
+            for gpu, values in peak_values.items():
+                formatted_output += f"\nGPU-{gpu} ({gpu_info[gpu]['Name']}):\n"
+                formatted_output += f"  UUID: {gpu_info[gpu]['UUID']}\n"
+                formatted_output += f"  Temperature: {values['Temperature']['Value']}°C (Time: {values['Temperature']['Time']})\n"
+                formatted_output += f"  Fan Speed: {values['Fan Speed']['Value']} % (Time: {values['Fan Speed']['Time']})\n"
+                formatted_output += f"  Power Draw: {values['Power Draw']['Value']} W (Time: {values['Power Draw']['Time']})\n"
+
+            # Write the formatted output to the file
+            with open(output_filename, "a") as log_file:
+                log_file.write(formatted_output)
+
+        time.sleep(120)
+
+if __name__ == "__main__":
+    main()
